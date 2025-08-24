6428fb1435c974e2fd5d650b1090f2e5d2657158
CHANDU-JANIPIREDDY <162881884+CHANDU-JANIPIREDDY@users.noreply.github.com>
2025-08-24 14:06:41 +0530

    Update Lists.jsx

--- admin/src/pages/Lists.jsx
+++ admin/src/pages/Lists.jsx
@@ -38,7 +38,7 @@ const removeList =async(id)=>{
       <Nav/>
       <div className='w-[100%] h-[100%] flex items-center justify-start'>
       <Sidebar/>
-      <div className='w-[82%] h-[100%] lg:ml-[320px] md:ml-[230px] mt-[70px] flex flex-col gap-[30px] overflow-x-hidden py-[50px] ml-[100px]'>
+      <div className='w-[82%] h-[100%] mt-[70px] flex flex-col gap-[30px] overflow-x-hidden py-[50px]'>
         <div className='w-[400px] h-[50px] text-[28px] md:text-[40px] mb-[20px] text-white'>All Listed Products</div>
 
         {
